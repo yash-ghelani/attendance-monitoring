d3a4de37f622ca8dfd8c40d0b85262347f99db70
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2021-03-22 22:52:50 +0530

    Added seeds for users and sessions, along with a rake command to output the timetabled_sessions as seed commands

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-superadmin = User.find_or_initialize_by(username: 'aca18mms', email: 'mmsarpatwar1@sheffield.ac.uk')
-superadmin.admin = true
-superadmin.save!
+
+users = [{"username" => 'aca18mms', "email" => 'mmsarpatwar1@sheffield.ac.uk', "admin" => true},
+                {"username" => 'aca18yg', "email" => 'yghelani1@sheffield.ac.uk', "admin" => true},
+                {"username" => 'aca18rf', "email" => 'rfaiz1@sheffield.ac.uk', "admin" => true},
+                {"username" => 'aca18ag', "email" => 'agoody1@sheffield.ac.uk', "admin" => true},
+                {"username" => 'acd19yc', "email" => 'ychen1@sheffield.ac.uk', "admin" => true},
+                {"username" => 'mea17nh', "email" => 'nheath2@sheffield.ac.uk', "admin" => true}]
+
+users.each do |user_details|
+    user = User.find_or_initialize_by({"username" => user_details['username'], "email" => user_details["email"]})
+    user.admin = user_details["admin"] ? true : false
+    user.lecturer = user_details["lecturer"] ? true : false
+    user.save!
+end
+
+TimetabledSession.first_or_create({"id"=>2, "session_code"=>"EFGH-5678", "session_title"=>"COM1008 - Javascript practical", "session_desc"=>"Anonymous functions and the arrow function", "department_code"=>"COM1008", "start_time"=>"2021-03-26T02:00:00.000Z", "end_time"=>"2021-03-26T03:00:00.000Z", "creator"=>1, "report_email"=>"", "created_at"=>"2021-03-22T16:34:58.823Z", "updated_at"=>"2021-03-22T16:34:58.823Z"})
+TimetabledSession.first_or_create({"id"=>3, "session_code"=>"IJKL-9101", "session_title"=>"COM2008 - Java MySQL connector tutorial", "session_desc"=>"Using MySQL adaptor with jdbc driver", "department_code"=>"COM2008", "start_time"=>"2021-03-29T12:00:00.000Z", "end_time"=>"2021-03-29T13:00:00.000Z", "creator"=>1, "report_email"=>"", "created_at"=>"2021-03-22T16:37:26.712Z", "updated_at"=>"2021-03-22T16:37:26.712Z"})
+TimetabledSession.first_or_create({"id"=>1, "session_code"=>"ABCD-1234", "session_title"=>"COM1001 - Practical", "session_desc"=>"Ruby tutorial", "department_code"=>"COM1001", "start_time"=>"2021-03-22T13:00:00.000Z", "end_time"=>"2021-03-22T14:00:00.000Z", "creator"=>1, "report_email"=>"", "created_at"=>"2021-03-22T16:33:43.635Z", "updated_at"=>"2021-03-22T16:37:51.490Z"})
