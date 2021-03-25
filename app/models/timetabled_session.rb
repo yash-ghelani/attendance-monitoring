e67ceda8dd8133825841fef8ea6d3375db88db8b
Nathanael <nheath2@sheffield.ac.uk>
2021-03-25 11:18:36 +0000

    Timetabled sessions generate random session code upon creation.

--- app/models/timetabled_session.rb
+++ app/models/timetabled_session.rb
@@ -16,4 +16,16 @@
 class TimetabledSession < ApplicationRecord
   has_many :session_attendances, foreign_key: :id
   has_many :users, through: :session_attendances
+  after_initialize :init
+
+  def generate_code(number)
+    charset = Array('A'..'Z') + Array('a'..'z') + Array(['0','1','2','3','4','5','6','7','8','9'])
+    Array.new(number) { charset.sample }.join
+  end
+
+  # generating random session code
+  def init
+    self.session_code ||= generate_code(8)  #(0...8).map { (65 + rand(26)).chr }.join
+  end
+
 end
