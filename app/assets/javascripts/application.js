b2a93e1b3e44f674582d30d6133424764cd6a0b6
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2021-04-21 10:07:02 +0530

    Added jquery datetimepicker fallback

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,3 +10,31 @@
 //= require select2
 //= require select2_init
 //= require cocoon
+//= require jquery-datetimepicker
+
+
+$(document).on('ready', function () {
+    $('.datetimepicker').each(function () {
+        const start = $("#start.datetimepicker");
+        const end = $("#end.datetimepicker");
+        if (this.type === 'text') {
+            // Native datepicker is not available
+            start.datetimepicker({
+                onShow: function (ct) {
+                    this.setOptions({
+                        maxDate: end.val() ? end.val() : false
+                    })
+                },
+            });
+            end.datetimepicker({
+                onShow: function (ct) {
+                    this.setOptions({
+                        minDate: start.val() ? start.val() : false
+                    })
+                },
+            });
+        }else if(this.type === 'date'){
+            // Add logic for native datetimepicker
+        }
+    })
+})
