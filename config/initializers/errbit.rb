2c5cfdb6e0ff6aa5acc4649863a72567decdea3b
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2021-04-30 18:31:35 +0530

    Revert "Initializing deployment configuration"
    This reverts commit 63f3073859ad13163cc8c4051193b97f928a4694.

--- config/initializers/errbit.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-    config.api_key = '52d3b811aab9113d96c35fafe07716f0'
-    config.host    = 'errbit.hut.shefcompsci.org.uk'
-    config.port    = 443
-    config.secure  = config.port == 443
-  end
