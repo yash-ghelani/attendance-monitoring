3ffae2e26ded53d99051d96a0b7c0280407dbe7c
R Faiz <rfaiz1@sheffield.ac.uk>
2021-05-05 16:35:28 +0100

    view history rspec test

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -25,4 +25,25 @@ describe 'Student Home Page' do
     click_button('Sign In')
     expect(page).to have_content "You're signed in"
   end
+
+  specify 'Visit History', js: true do
+    student = FactoryBot.create(:student)
+    lecturer = FactoryBot.create(:lecturer)
+    session = FactoryBot.build(:session, creator: lecturer)
+    session.save
+    login_as student
+    visit '/'
+    session.session_code.split('').each_with_index do |val, i|
+      fill_in "code-#{i+1}", with: val
+    end
+    wait_for_ajax
+    expect(page).to have_content session.session_title
+    expect(page).to have_content session.module_code
+
+    click_button('Sign In')
+    expect(page).to have_content "You're signed in"
+    visit '/student/history'
+    expect(page).to have_content session.session_title
+
+  end
 end
