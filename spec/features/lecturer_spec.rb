4dd7a70afce12cb93d1d452ef3f0858cba989348
yashghelani <yghelani1@sheffield.ac.uk>
2021-05-05 17:42:07 +0100

    lecturer rspec test - view session on dash

--- spec/features/lecturer_spec.rb
+++ spec/features/lecturer_spec.rb
@@ -51,4 +51,54 @@ describe 'Lecturer Home Page' do
 
   end
 
+  specify 'Create new session with a registered lecturer', js: true do
+    lecturer1 = FactoryBot.create(:lecturer)
+    lecturer2 = FactoryBot.create(:lecturer)
+    lecturer3 = FactoryBot.create(:lecturer)
+    lecturer4 = FactoryBot.create(:lecturer)
+
+    login_as lecturer1
+    visit '/'
+    click_link('New Session')
+    fill_in 'Session title', with: 'HTML Technologies'
+    fill_in 'Module code', with: 'COM1008'
+    page.execute_script("$('#start_time_picker').val(arguments[0]).change()", Time.now.strftime('%Y-%m-%dT%H:%M'))
+    page.execute_script("$('#end_time_picker').val(arguments[0]).change()",(Time.now+30.minutes).strftime('%Y-%m-%dT%H:%M'))
+    
+    click_link('Register Lecturer')
+    lecturer_select_box = all("#new_timetabled_session .nested-fields").last.find('select').click
+    lecturer_select_box.find('option', :text => lecturer3.email).click
+    
+    click_button('Create Timetabled session')
+
+    visit '/'
+    session = lecturer1.timetabled_sessions.last
+    find("#open-close-#{session.id}").click
+    page.find("#accordian-#{session.id}").should have_text(lecturer1.email)
+    page.find("#accordian-#{session.id}").should have_text(lecturer3.email)
+
+  end
+
+  specify 'View session in dashboard', js: true do
+    lecturer = FactoryBot.create(:lecturer)
+    login_as lecturer
+    visit '/'
+    
+    click_link('New Session')
+    
+    fill_in 'Session title', with: 'Demo Session'
+    fill_in 'Module code', with: 'COM1234'
+    page.execute_script("$('#start_time_picker').val(arguments[0]).change()", Time.now+60.minutes.strftime('%Y-%m-%dT%H:%M'))
+    page.execute_script("$('#end_time_picker').val(arguments[0]).change()",(Time.now+120.minutes).strftime('%Y-%m-%dT%H:%M'))
+    
+    click_button('Create Timetabled session')
+
+    visit '/'
+
+    expect(page).to have_content 'Welcome to COM attendance, from here you can view sessions as well as create new ones.'
+    expect(page).to have_content 'Demo Session'
+    expect(page).to have_content 'COM1234'
+
+  end
+
 end
