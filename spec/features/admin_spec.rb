0a0b2d2f00a5057075c852282e06f495e68a217f
yashghelani <yghelani1@sheffield.ac.uk>
2021-05-19 17:21:16 +0100

    admin check session attendance

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -76,4 +76,53 @@ describe 'Admin Home Page' do
 
   end
 
+  specify 'Check attendance monitoring in show attendance', js: true do
+    student = FactoryBot.create(:student)
+    admin = FactoryBot.create(:admin)
+
+    login_as admin
+    visit '/'
+    
+    click_link('New Session')
+    
+    fill_in 'Session title', with: 'Demo Session'
+    fill_in 'Module code', with: 'COM1234'
+    page.execute_script("$('#start_time_picker').val(arguments[0]).change()", Time.now.strftime('%Y-%m-%dT%H:%M'))
+    page.execute_script("$('#end_time_picker').val(arguments[0]).change()",(Time.now+120.minutes).strftime('%Y-%m-%dT%H:%M'))
+    
+    click_button('Create Timetabled session')
+    expect(page).to have_content 'COM1234'
+    visit '/'
+
+    expect(page).to have_content 'Welcome to COM attendance, from here you can view sessions as well as create new ones.'
+    expect(page).to have_content 'Demo Session'
+    expect(page).to have_content 'COM1234'
+
+    page.find("#open-close-1").click
+    click_on(class: 'btn btn-light-green btn-block h-100')
+    expect(page).to have_content 'Here you can present your session code and see students that join the session'
+    ses_code = find(:xpath, '//*[@id="main-container"]/div/div[3]/div/div[1]/div[1]/h2/b').text
+    
+
+    login_as student
+    visit '/'
+    ses_code.split('').each_with_index do |val, i|
+      fill_in "code-#{i+1}", with: val
+    end
+    wait_for_ajax
+    expect(page).to have_content 'Demo Session'
+    expect(page).to have_content 'COM1234'
+    click_button('Sign In')
+    expect(page).to have_content "You're signed in"
+    
+
+    login_as admin
+    visit '/'
+    expect(page).to have_content 'Welcome to COM attendance, from here you can view sessions as well as create new ones.'
+    page.find("#open-close-1").click
+    click_on(class: 'btn btn-light-blue btn-block h-100')
+    expect(page).to have_content 'Sign In Time'
+
+  end
+
 end
