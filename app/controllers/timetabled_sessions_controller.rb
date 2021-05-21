7e1e922544bd0a439687d4922613e9347b10de3c
Nathanael <nheath2@sheffield.ac.uk>
2021-05-21 10:02:23 +0100

    Removed characters from randomly generated string to remove possibility for profanities

--- app/controllers/timetabled_sessions_controller.rb
+++ app/controllers/timetabled_sessions_controller.rb
@@ -107,7 +107,7 @@ class TimetabledSessionsController < ApplicationController
     end
 
     def generate_code(number)
-      charset = Array('A'..'Z') + Array('a'..'z') + Array(['0','1','2','3','4','5','6','7','8','9'])
+      charset = Array('B','C','D','F','G','H','J','K','M','P','Q','R','T','V','W','X','Y') + Array('a'..'z') + Array(['3','4','6','7','8','9'])
       Array.new(number) { charset.sample }.join
     end
 
